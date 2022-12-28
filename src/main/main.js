7163319d745ad36602370c6a9752aeaa257492e9
Liuyuhang610065 <71393287+Liuyuhang610065@users.noreply.github.com>
2022-12-28 15:38:19 +0800

    轨道控制器和坐标轴

--- src/main/main.js
+++ src/main/main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
-
+// 导入轨道控制器
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 // console.log(THREE)
 
 const scene = new THREE.Scene()
@@ -23,5 +24,21 @@ scene.add(cube)
 const renderer = new THREE.WebGLRenderer()
 // 设置渲染的尺寸大小
 renderer.setSize(window.innerWidth, window.innerHeight)
+// 将渲染内容添加到body
+document.body.appendChild(renderer.domElement)
+renderer.render(scene, camera)
+
+// 创建轨道控制器
+const controls = new OrbitControls(camera, renderer.domElement)
 
 
+// 添加坐标轴辅助器
+const axes = new THREE.AxesHelper(5)
+scene.add(axes)
+
+function render() {
+    renderer.render(scene, camera)
+    requestAnimationFrame(render)
+}
+
+render()
