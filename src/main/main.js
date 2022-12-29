24ff3679da1f96a7c5597d348b3e4d8fea46d1ed
Liuyuhang610065 <71393287+Liuyuhang610065@users.noreply.github.com>
2022-12-29 19:06:14 +0800

    gui

--- src/main/main.js
+++ src/main/main.js
@@ -2,6 +2,8 @@ import * as THREE from 'three'
 // 导入轨道控制器
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 // console.log(THREE)
+import gsap from 'gsap'
+import * as dat from 'dat.gui'
 
 const scene = new THREE.Scene()
 
@@ -30,14 +32,14 @@ renderer.render(scene, camera)
 
 // 创建轨道控制器
 const controls = new OrbitControls(camera, renderer.domElement)
-
+controls.enableDamping = true // 阻尼
 
 // 添加坐标轴辅助器
 const axes = new THREE.AxesHelper(5)
 scene.add(axes)
 
 // 修改物体位置
-cube.position.set(5, 0, 0)
+cube.position.set(0, 0, 0)
 cube.position.y = 5
 
 // 缩放
@@ -46,13 +48,90 @@ cube.scale.set(1, 0.5, 2)
 // 旋转
 cube.rotation.set(Math.PI / 4, 0, 0)
 
+// 时钟
+const clock = new THREE.Clock()
+
+// 设置动画
+// var animation = gsap.to(cube.position, {
+//     x:5, 
+//     duration:5, 
+//     ease:'power1.inOut', 
+//     onComplete:() => {
+        
+//     },
+//     onStart: () => {
+
+//     },
+//     repeat: 2, // -1无限循环，
+//     yoyo: true, // 往返
+//     delay: 2, //延迟
+// })
+
+// 动画控制
+// window.addEventListener('dblclick', ()=>{
+//     if(animation.isActive()){
+//         animation.pause()
+//     } else {
+//         animation.resume()
+//     }
+// })
+
+// 监听画面变化
+window.addEventListener("resize", ()=>{
+    // 更新宽高比
+    camera.aspect = window.innerWidth / window.innerHeight
+    // 更新投影矩阵
+    camera.updateProjectionMatrix()
+    // 更新渲染器
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    // 设置像素比
+    renderer.setPixelRatio(window.devicePixelRatio)
+})
+
+// 控制全屏
+// renderer.domElement.requestFullscreen()
+// document.exitFullscreen()
 
 function render() {
-    cube.position.x += 0.01
-    cube.rotateX(0.01)
-    if(cube.position.x > 5) cube.position.x = 0
+    // let t = time/1000 % 5
+    // cube.position.x = 1*t
+    // cube.rotateX(1*t)
+
+    // let time = clock.getElapsedTime()
+    // let deltaTime = clock.getDelta()
+    // cube.position.x = time % 5
+    controls.update()
     renderer.render(scene, camera)
     requestAnimationFrame(render)
 }
 
 render()
+
+
+// gui控制
+const gui = new dat.GUI()
+gui
+    .add(cube.position, 'x')
+    .min(0)
+    .max(5)
+    .step(0.01)
+    .name('移动x轴')
+    .onChange()
+    .onFinishChange()
+const params = {
+    color: '#ffff00',
+    fn: () => {
+        gsap.to(cube.position, {x:5, duration:2, yoyo:true})
+    }
+}
+// 控制颜色
+gui.addColor(params, 'color').onChange((value) => {
+    cube.material.color.set(value)
+})
+// 控制显示
+gui.add(cube, 'visible')
+// 触发事件
+gui.add(params, 'fn').name('运动')
+// 文件夹
+var folder = gui.addFolder('设置')
+folder.add(cube.material, 'wireframe')
